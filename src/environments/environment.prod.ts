4878b6d39a52807294ea077888d47d778455201c
Filipe Veber <pis0507@supero.com.br>
2022-04-21 14:35:47 -0300

    Adding production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  envName: 'PRODUCTION',
+  urlApiV1: 'http://localhost:3000/',
+  urlAuthApiBack: 'https://nuki-brecho-app.herokuapp.com/api/',
 };
