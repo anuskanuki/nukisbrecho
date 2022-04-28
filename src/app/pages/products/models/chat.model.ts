a1f1fc5eab3398ea877fdb5e1d698961ecf80a68
Filipe Veber <pis0507@supero.com.br>
2022-04-28 18:51:49 -0300

    Chat working

--- src/app/pages/products/models/chat.model.ts
+++ src/app/pages/products/models/chat.model.ts
@@ -1,11 +1,6 @@
 export class ChatModel {
     public id?: string;
     public productId?: string;
-    public questions?: QuestionModel[];
-}
-
-export class QuestionModel {
-    public questionId?: string;
     public question?: string;
     public answer?: string;
 }
