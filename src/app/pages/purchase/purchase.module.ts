3d5320463877cfaf5389d0e540a2dff7b473ced7
anuska.rehn <anuska.rehn@ambevtech.com.br>
2022-05-01 00:59:40 -0300

    forbidden reloading purchase

--- src/app/pages/purchase/purchase.module.ts
+++ src/app/pages/purchase/purchase.module.ts
@@ -4,10 +4,12 @@ import { PurchaseRoutingModule } from './purchase-routing.module';
 import { PurchaseComponent } from './purchase/purchase.component';
 import { NgZorroAntdModule } from 'src/app/core/modules/ng-zorro-antd.module';
 import { CartComponent } from './cart/cart.component';
+import { ServerErrosPagesModule } from '../server-errors/serverErrosPages.module';
 
 @NgModule({
   declarations: [PurchaseComponent, CartComponent],
   imports: [
+    ServerErrosPagesModule,
     CommonModule,
     NgZorroAntdModule,
     PurchaseRoutingModule
